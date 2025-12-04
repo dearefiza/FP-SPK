fb2060aa1f658cb0c513cbdba7abca511d447ce6
“adelia” <22082010034@student.upnjatim.ac.id>
2025-12-04 12:45:45 +0700

    pesan commit kamu, misal: tambah fitur divisi karyawan

--- page/barang.php
+++ page/barang.php
@@ -3,8 +3,8 @@
     <div class="panel-middle" id="judul">
         <img src="asset/image/barang.svg">
         <div id="judul-text">
-            <h2 class="text-green">BARANG</h2>
-            Halamanan Administrator Barang
+            <h2 class="text-green">Karyawan</h2>
+            Halamanan Administrator Karyawan
         </div>
     </div>
 </div>
@@ -21,35 +21,60 @@
             ?>
         </div>
     </div>
+
     <div class="col-8">
         <div class="panel">
             <div class="panel-top">
-                <b class="text-green">Daftar Barang</b>
+                <b class="text-green">Daftar Karyawan</b>
             </div>
             <div class="panel-middle">
                 <div class="table-responsive">
                     <table>
-                        <thead><tr><th>No</th><th>Nama</th><th>Aksi</th></tr></thead>
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama</th>
+                                <th>Divisi</th>  <!-- DITAMBAHKAN -->
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
                         <tbody>
                         <?php
-                        $query="SELECT * FROM jenis_barang";
+                        // JOIN ke tabel divisi
+                        $query="
+                        SELECT karyawan.*, divisi.nama_divisi 
+                        FROM karyawan 
+                        LEFT JOIN divisi ON karyawan.id_divisi = divisi.id_divisi
+                        ";
+
                         $execute=$konek->query($query);
+
                         if ($execute->num_rows > 0){
                             $no=1;
                             while($data=$execute->fetch_array(MYSQLI_ASSOC)){
                                 echo"
                                 <tr id='data'>
                                     <td>$no</td>
-                                    <td>$data[namaBarang]</td>
+                                    <td>$data[nama_karyawan]</td>
+                                    <td>$data[nama_divisi]</td>   <!-- TAMPILKAN DIVISI -->
                                     <td>
-                                    <div class='norebuttom'>
-                                    <a class=\"btn btn-light-green\" href='./?page=barang&aksi=ubah&id=".$data['id_jenisbarang']."'><i class='fa fa-pencil-alt'></i></a>
-                                    <a class=\"btn btn-yellow\" data-a=".$data['namaBarang']." id='hapus' href='./proses/proseshapus.php/?op=barang&id=".$data['id_jenisbarang']."'><i class='fa fa-trash-alt'></i></a></td>
-                                </div></tr>";
+                                        <div class='norebuttom'>
+                                            <a class='btn btn-light-green' href='./?page=barang&aksi=ubah&id=".$data['id_karyawan']."'>
+                                                <i class='fa fa-pencil-alt'></i>
+                                            </a>
+                                            <a class='btn btn-yellow' 
+                                               data-a='".$data['nama_karyawan']."' 
+                                               id='hapus' 
+                                               href='./proses/proseshapus.php/?op=barang&id=".$data['id_karyawan']."'>
+                                               <i class='fa fa-trash-alt'></i>
+                                            </a>
+                                        </div>
+                                    </td>
+                                </tr>";
                                 $no++;
                             }
-                        }else{
-                            echo "<tr><td  class='text-center text-green' colspan='3'>Kosong</td></tr>";
+                        } else {
+                            echo "<tr><td class='text-center text-green' colspan='4'>Kosong</td></tr>";
                         }
                         ?>
                         </tbody>
